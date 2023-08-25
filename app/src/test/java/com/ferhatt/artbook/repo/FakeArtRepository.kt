de996268e019abfacd58390c689189f12eb2c46c
ferhatseker180 <ferhatseker180@gmail.com>
2023-08-26 00:24:37 +0300

    Ana Fragment Oluşturuldu, dizayn edildi.Xml dosyaları tasarlandı.Binding ve Navigation işlemleri halledildi. Room Database Oluşturuldu,Modellemeler yapıldı,Retrofit yapısı oluşturuldu ve Dagger Hilt Hazırlandı.ViewModel yapılandırıldı.Viewlar ayarlandı. Uygulama tamamlandı. View Model ve Live Data Testleri Yapıldı.

---
+++ app/src/test/java/com/ferhatt/artbook/repo/FakeArtRepository.kt
@@ -0,0 +1,39 @@
+package com.ferhatt.artbook.repo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.ferhatt.artbook.model.ImageResponse
+import com.ferhatt.artbook.roomdb.Art
+import com.ferhatt.artbook.util.Resource
+
+class FakeArtRepository : ArtRepoInterface {
+
+    private val arts = mutableListOf<Art>()
+    private val artsLiveData = MutableLiveData<List<Art>>(arts)
+
+    override suspend fun insertArt(art: Art) {
+
+        arts.add(art)
+        refreshData()
+    }
+
+    override suspend fun deleteArt(art: Art) {
+
+        arts.remove(art)
+        refreshData()
+    }
+
+    override fun getArt(): LiveData<List<Art>> {
+
+        return artsLiveData
+    }
+
+    override suspend fun searchImage(imageString: String): Resource<ImageResponse> {
+
+        return Resource.success(ImageResponse(listOf(),0,0))
+    }
+
+    private fun refreshData(){
+        artsLiveData.postValue(arts)
+    }
+}
