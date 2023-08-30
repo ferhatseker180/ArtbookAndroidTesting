d35ed4c5f54a92f894f0ad002aeeddf944c118f7
ferhatseker180 <ferhatseker180@gmail.com>
2023-08-30 17:21:14 +0300

    Ana Fragment Oluşturuldu, dizayn edildi.Xml dosyaları tasarlandı.Binding ve Navigation işlemleri halledildi. Room Database Oluşturuldu,Modellemeler yapıldı,Retrofit yapısı oluşturuldu ve Dagger Hilt Hazırlandı.ViewModel yapılandırıldı.Viewlar ayarlandı. Uygulama tamamlandı. View Model ve Live Data Testleri Yapıldı. Unit Testler bitirildi, UI Test aşamasına geçilecek. Espresso ve Mockito ile UI testleri gerçekleştirildi.

---
+++ app/src/androidTest/java/com/ferhatt/artbook/view/ArtDetailsFragmentTest.kt
@@ -0,0 +1,111 @@
+package com.atilsamancioglu.artbookhilttesting.view
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.fragment.app.FragmentFactory
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.ferhatt.artbook.R
+import com.ferhatt.artbook.getOrAwaitValue
+import com.ferhatt.artbook.launchFragmentInHiltContainer
+import com.ferhatt.artbook.repo.FakeArtRepositoryAndroidTest
+import com.ferhatt.artbook.roomdb.Art
+import com.ferhatt.artbook.view.ArtDetailsFragment
+import com.ferhatt.artbook.view.ArtDetailsFragmentDirections
+import com.ferhatt.artbook.view.ArtFragmentFactory
+import com.ferhatt.artbook.viewmodel.ArtViewModel
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import javax.inject.Inject
+
+@MediumTest
+@HiltAndroidTest
+@ExperimentalCoroutinesApi
+class ArtDetailsFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory : ArtFragmentFactory
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun testNavigationFromArtDetailsToImageAPI() {
+        val navController = Mockito.mock(NavController::class.java)
+
+        launchFragmentInHiltContainer<ArtDetailsFragment>(
+            factory = fragmentFactory
+        ) {
+            Navigation.setViewNavController(requireView(),navController)
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.artImageView)).perform(ViewActions.click())
+        Mockito.verify(navController).navigate(
+            ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment()
+        )
+    }
+
+    @Test
+    fun testOnBackPressed() {
+        val navController = Mockito.mock(NavController::class.java)
+        launchFragmentInHiltContainer<ArtDetailsFragment>(
+            factory = fragmentFactory
+        ) {
+            Navigation.setViewNavController(requireView(),navController)
+        }
+
+        pressBack()
+        verify(navController).popBackStack()
+    }
+
+    @Test
+    fun testSave(){
+        val testViewModel = ArtViewModel(FakeArtRepositoryAndroidTest())
+
+        launchFragmentInHiltContainer<ArtDetailsFragment>(
+            factory = fragmentFactory
+        ){
+            viewModel = testViewModel
+        }
+
+       Espresso.onView(withId(R.id.nameText)).perform(replaceText("Mona Lisa"))
+        Espresso.onView(withId(R.id.artistText)).perform(replaceText("Leonardo Da Vinci"))
+        Espresso.onView(withId(R.id.yearText)).perform(replaceText("1700"))
+        Espresso.onView(withId(R.id.saveButton)).perform(click())
+
+        assertThat(testViewModel.artList.getOrAwaitValue()).contains(
+            Art(
+                "Mona Lisa",
+                "Leonardo Da Vinci",
+                1700,
+                "")
+        )
+
+    }
+
+
+
+}
